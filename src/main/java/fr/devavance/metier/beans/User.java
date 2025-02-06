9c350993704dbdd6298e09c8ffac37d5bb791ec7
mohamed.rahmani <bouchkhaf@gmail.com>
2025-02-06 14:06:39 +0100

    Question 1: Compléter la classe User pour que celle-ci soit considérée comme un bean.

--- src/main/java/fr/devavance/metier/beans/User.java
+++ src/main/java/fr/devavance/metier/beans/User.java
@@ -12,7 +12,38 @@ public class User {
     private String profil = "";  // Profil de l'utilisateur "admin" ou "user"
     private Boolean auth = false; // Important : déconnecté par défaut
 
-    
+    public User() {
+    }
 
+    public String getUserName() {
+        return userName;
+    }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfil() {
+        return profil;
+    }
+
+    public void setProfil(String profil) {
+        this.profil = profil;
+    }
+
+    public Boolean getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Boolean auth) {
+        this.auth = auth;
+    }
 }
